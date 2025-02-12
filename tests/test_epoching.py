f643b0519ad84e2a7a37d3535ec2b6742991bb39
matthiasdold <matthias.dold@gmx.net>
2025-02-12 20:02:02 +0100

    updated test

--- tests/test_epoching.py
+++ tests/test_epoching.py
@@ -6,34 +6,31 @@ import pytest
 from erp_classifier.context import get_context
 from erp_classifier.feature_extraction import get_new_epochs
 from erp_classifier.logging import logger
-from tests.shared import spawn_lsl_marker_stream, spawn_lsl_stream
+from tests.shared import ctx, spawn_lsl_marker_stream, spawn_lsl_stream
 
 logger.setLevel("INFO")
 
 
-def test_epoching(spawn_lsl_stream, spawn_lsl_marker_stream):
+def test_epoching(ctx, spawn_lsl_stream, spawn_lsl_marker_stream):
 
-    ctx = get_context()
     ctx.decode_trigger_markers = [101, 102, 103, 104]
     ctx.connect_input(stream_name="test", marker_stream_name="markers")
 
     start = time.perf_counter()
 
-    epochs_accumulating_stack = []  # stack to collect currently accumulating epochs
-    epochs_for_clf_stack = []
     while time.perf_counter() - start < 2:
 
         # get new data into the StreamWatchers and pass through the filter_bank
         ctx.update_stream_watchers()
 
-        epochs_accumulating_stack.extend(get_new_epochs(ctx))
+        ctx.epochs_accumulating_stack.extend(get_new_epochs(ctx))
 
         n_added = 0
         curr_data = None
         curr_ts = None
 
         # check if we already have completed epochs
-        while epochs_accumulating_stack != []:
+        while ctx.epochs_accumulating_stack != []:
             if curr_data is None and curr_ts is None:
                 # logger.debug(f"Looking at {ctx.filter_bank.n_new=}")
 
@@ -44,7 +41,7 @@ def test_epoching(spawn_lsl_stream, spawn_lsl_marker_stream):
 
                 curr_data = ctx.filter_bank.get_data()[-ctx.filter_bank.n_new :]
 
-            epo = epochs_accumulating_stack[0]
+            epo = ctx.epochs_accumulating_stack[0]
 
             # if enough data -> fill, pop epoch, and add to the classifier stack
             if (epo.ts_event + epo.tmax) < curr_ts[-1]:
@@ -58,7 +55,7 @@ def test_epoching(spawn_lsl_stream, spawn_lsl_marker_stream):
                     curr_ts[idx : idx + epo.nsamples],
                 )
 
-                epochs_for_clf_stack.append(epochs_accumulating_stack.pop(0))
+                ctx.epochs_for_clf_stack.append(ctx.epochs_accumulating_stack.pop(0))
                 n_added = max(n_added, idx + 1)
 
             # end time for first epoch not yet reached, nothing to do for now
@@ -70,20 +67,16 @@ def test_epoching(spawn_lsl_stream, spawn_lsl_marker_stream):
         )
 
         # Not subject of this test -> here would be the decoding part
-        for epo in epochs_for_clf_stack:
+        for epo in ctx.epochs_for_clf_stack:
             pass
 
         time.sleep(ctx.dt_s)
 
     # we should have 3 full epochs at this point
-    assert len(epochs_for_clf_stack) == 3
+    assert len(ctx.epochs_for_clf_stack) == 3
     assert (
-        np.abs(np.diff([e.ts_event for e in epochs_for_clf_stack]).mean() - 0.25) < 0.05
+        np.abs(np.diff([e.ts_event for e in ctx.epochs_for_clf_stack]).mean() - 0.25)
+        < 0.05
     )
     # marker stream for testing is created to have consecutive markers always with diff = +/-1
-    assert (np.abs(np.diff([e.event for e in epochs_for_clf_stack])) == 1).all()
-
-
-def test_feature_extraction(ctx):
-
-    pass
+    assert (np.abs(np.diff([e.event for e in ctx.epochs_for_clf_stack])) == 1).all()
